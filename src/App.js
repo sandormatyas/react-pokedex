76450967dfe5d1b2e854a80003386f2d44bceb71
sandormatyas <sandormatyas7@gmail.com>
2020-03-18 17:15:56 +0100

    make api call to pokeapi and store it in state when starting app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import PokemonList from "./components/PokemonList";
+import axios from "axios";
 
-function App() {
-  return <div>Hello</div>;
+export class App extends Component {
+  componentDidMount() {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then(pokemonList =>
+      pokemonList.data.results.map(singlePokemon =>
+        axios.get(singlePokemon.url).then(pokemonDetails =>
+          this.setState(currentState => ({
+            pokemonList: [...currentState.pokemonList, pokemonDetails.data]
+          }))
+        )
+      )
+    );
+  }
+
+  state = {
+    pokemonList: []
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <PokemonList pokemonList={this.state.pokemonList} />
+      </div>
+    );
+  }
 }
 
 export default App;
